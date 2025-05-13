71661509e1f7fd923b64816981f579051b5db922
Vimal Kumar Yadav <vimalyadavkr001@gmail.com>
2025-05-13 13:06:35 +0530

    Initial commit

---
+++ src/main/java/in/org/bricks/ai/lms/Bricks/BricksApplication.java
@@ -0,0 +1,13 @@
+package in.org.bricks.ai.lms.Bricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/org/bricks/ai/lms/Bricks/BricksApplicationTests.java
@@ -0,0 +1,13 @@
+package in.org.bricks.ai.lms.Bricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
